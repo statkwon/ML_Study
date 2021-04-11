02a135ab56dec01c5797b149ff139dbe56b3fb4f
statkwon <57558636+statkwon@users.noreply.github.com>
2021-04-11 15:49:08 +0900

    Add files via upload

--- MyLogisticRegression.py
+++ MyLogisticRegression.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class MyLogisticRegression:
-    def __init__(self, max_iter=100):
+    def __init__(self, max_iter=10):
         self.max_iter = max_iter
     def fit(self, X_train, y_train):
         X_train = np.array(X_train)
@@ -11,7 +11,7 @@ class MyLogisticRegression:
             p1 = np.exp(X_train.dot(beta))/(1+np.exp(X_train.dot(beta)))
             p0 = 1-p1
             W = np.diag(p0*p1)
-            beta = np.linalg.inv(np.transpose(X_train).dot(W).dot(X_train)).dot(np.transpose(X_train)).dot(y_train-p1)
+            beta = beta + np.linalg.inv(np.transpose(X_train).dot(W).dot(X_train)).dot(np.transpose(X_train)).dot(y_train-p1)
             self.beta_new = beta
     def predict(self, X_test):
         X_test = np.array(X_test)
